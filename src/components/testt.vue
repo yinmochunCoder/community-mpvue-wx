2c3daef5c8fee0e8c9756b53c99450f0b961ec83
肖艳丽 <01532410@yto.net.cn>
2021-07-20 15:51:50 +0800

    first commit

---
+++ src/components/testt.vue
@@ -0,0 +1,13 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+  export default {
+    name: 'testt'
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,100 @@
+<template>
+  <div @click="clickHandle">
+    121212121212
+    <Icon :size="'120'" :color="'green'" :src="'/static/icons/camera.svg'"></Icon>
+    <card></card>
+  </div>
+</template>
+
+<script>
+import Icon from '@/components/icon/index'
+import card from '@/components/card'
+export default {
+  data () {
+    return {
+      motto: 'Hello miniprograme',
+      userInfo: {
+        nickName: 'mpvue',
+        avatarUrl: 'http://mpvue.com/assets/logo.png'
+      }
+    }
+  },
+
+  components: {
+    Icon,
+    card
+  },
+
+  methods: {
+    bindViewTap () {
+      const url = '../logs/main'
+      if (mpvuePlatform === 'wx') {
+        mpvue.switchTab({ url })
+      } else {
+        mpvue.navigateTo({ url })
+      }
+    },
+    clickHandle (ev) {
+      console.log('clickHandle:', ev)
+      // throw {message: 'custom test.vue'}
+    }
+  },
+
+  created () {
+    // let app = getApp()
+  }
+}
+</script>
+
+<style scoped>
+.userinfo {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.userinfo-avatar {
+  width: 128rpx;
+  height: 128rpx;
+  margin: 20rpx;
+  border-radius: 50%;
+}
+
+.userinfo-nickname {
+  color: #aaa;
+}
+
+.usermotto {
+  margin-top: 150px;
+}
+
+.form-control {
+  display: block;
+  padding: 0 12px;
+  margin-bottom: 5px;
+  border: 1px solid #ccc;
+}
+.all{
+  width:7.5rem;
+  height:1rem;
+  background-color:blue;
+}
+.all:after{
+  display:block;
+  content:'';
+  clear:both;
+}
+.left{
+  float:left;
+  width:3rem;
+  height:1rem;
+  background-color:red;
+}
+
+.right{
+  float:left;
+  width:4.5rem;
+  height:1rem;
+  background-color:green;
+}
+</style>
